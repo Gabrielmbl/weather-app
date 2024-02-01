4ca1441c5bbea65be700ec455f7dce75fd178d74
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-02-01 18:21:36 +0000

    Debugging GeocodingService

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -1,10 +1,15 @@
+require 'uri'
+require 'net/http'
+require 'json'
+require 'uri'
+
 class GeocodingService
   def initialize(address)
     @address = address
   end
 
   def geocode
-    uri = URI("https://geocode.xyz/#{URI.encode(@address)}?json=1&auth=182754041308105193597x90166")
+    uri = URI.parse("https://geocode.xyz/#{URI.encode_www_form_component(@address)}?json=1&auth=182754041308105193597x90166")
     response = Net::HTTP.get(uri)
     JSON.parse(response)
   end
