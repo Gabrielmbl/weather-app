dd5971d875880cbe5c64ebb6705a46604e94f8d4
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-02-02 01:54:20 +0000

    Added /weather_app/test/integration/ip_api_integration_test.rb which tests the response from the ip api

---
+++ test/integration/ip_api_integration_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+require 'vcr'
+require 'uri'
+
+class IpApiIntegrationTest < ActionDispatch::IntegrationTest
+  test 'should retrieve data from IP API' do
+    VCR.use_cassette('ip_api_interaction') do
+      ip_service = IpInfoService.new('8.8.8.8')
+      ip_info = ip_service.get_info
+
+      expected_data = <<~JSON
+        {
+          "ip": "8.8.8.8",
+          "network": "8.8.8.0/24",
+          "version": "IPv4",
+          "city": "Mountain View",
+          "region": "California",
+          "region_code": "CA",
+          "country": "US",
+          "country_name": "United States",
+          "country_code": "US",
+          "country_code_iso3": "USA",
+          "country_capital": "Washington",
+          "country_tld": ".us",
+          "continent_code": "NA",
+          "in_eu": false,
+          "postal": "94043",
+          "latitude": 37.42301,
+          "longitude": -122.083352,
+          "timezone": "America/Los_Angeles",
+          "utc_offset": "-0800",
+          "country_calling_code": "+1",
+          "currency": "USD",
+          "currency_name": "Dollar",
+          "languages": "en-US,es-US,haw,fr",
+          "country_area": 9629091.0,
+          "country_population": 327167434,
+          "asn": "AS15169",
+          "org": "GOOGLE"
+      }
+      JSON
+
+      assert_equal JSON.parse(expected_data), ip_info
+    end
+  end
+end
